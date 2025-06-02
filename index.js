a1c9d5479f73a3f072822592a985eba84b33a1fc
ola <olaidealaka12@gmail.com>
2025-06-02 10:50:36 +0100

    feature: Add buttons for user choice
    User interaction is included by styling,
    adding buttons and displaying scores

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const choiceButtons = document.querySelectorAll(".option_btn");
+const humanChoice = document.querySelector(".human_choice");
+const computerChoice = document.querySelector(".computer_choice");
+const results = document.querySelector(".results");
+const humanScoreText = document.querySelector(".human_score");
+const computerScoreText = document.querySelector(".computer_score");
+const finalScore = document.querySelector(".final_score");
+const ROUNDS = 5
 let humanScore = 0;
 let computerScore = 0;
 
@@ -12,37 +20,70 @@ const getHumanChoice = () => {
     return choice.toLowerCase()
 }
 
+choiceButtons.forEach(choice => {
+    choice.addEventListener("click", (e)=>{
+        let humanSelection = e.target.textContent.toLowerCase();
+        let computerSelection = getComputerChoice();
+        humanChoice.textContent = humanSelection;
+        computerChoice.textContent = computerSelection;
+        playRound(humanSelection, computerSelection);
+        }
+    )
+})
+
 const playRound = (humanChoice, computerChoice) => {
     if(humanChoice=="rock" && computerChoice =="scissors"){
         humanScore++;
-        console.log("You win!")
+        results.textContent = "You win!";
     }
     else if(humanChoice=="scissors" && computerChoice =="rock"){
         computerScore++;
-        console.log("You lose! Rock beats Scissors.")
+        results.textContent = "You lose! Rock beats Scissors.";
 
     }
     else if(humanChoice=="scissors" && computerChoice =="paper"){
         humanScore++;
-        console.log("You win!")
+        results.textContent = "You win!";
     }
     else if(humanChoice=="paper" && computerChoice =="scissors"){
         computerScore++;
-        console.log("You lose! Scissors beats Paper.")
+        results.textContent = "You lose! Scissors beats Paper.";
     }
     else if(humanChoice=="paper" && computerChoice =="rock"){
         humanScore++;
-        console.log("You win!")
+        results.textContent = "You win!";
     }
     else if(humanChoice=="rock" && computerChoice =="paper"){
         computerScore++;
-        console.log("You lose! Paper beats Rock.")
+        results.textContent = "You lose! Paper beats Rock.";
     }
     else {
-        console.log("You draw")
+        results.textContent = "You draw";
+    }
+    humanScoreText.textContent = humanScore;
+    computerScoreText.textContent = computerScore;
+    showFinalScore()
+}
+
+const getWinner = () => {
+    if(humanScore > computerScore) {
+        finalScore.textContent = "You got more points. You win!";
+    }
+    else if(humanScore < computerScore) {
+        finalScore.textContent = "You got fewer points. Computer wins!";
+    }
+    else {
+        finalScore.textContent = "You draw!";
     }
 }
 
+const showFinalScore = () => {
+    if(humanScore == ROUNDS || computerScore == ROUNDS) {
+        getWinner();
+        humanScore = 0;
+        computerScore = 0;
+    }
+}
 
 const playGame = () => {
     let totalRounds = 5;
@@ -52,15 +93,6 @@ const playGame = () => {
         console.info(humanSelection, computerSelection);
         playRound(humanSelection, computerSelection);
     }
-    if(humanScore > computerScore) {
-        console.log("You got more points. You win!")
-    }
-    else if(humanScore < computerScore) {
-        console.log("You got fewer points. Computer wins!")
-    }
-    else {
-        console.log("You draw!")
-    }
 }
 
-playGame();
+// playGame();
